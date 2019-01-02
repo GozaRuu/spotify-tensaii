b82f7934eb3028459854bc7e7aea3f5b9b52d03a
GozaRuu <kais.sghari@gmail.com>
2019-01-02 12:41:00 +0100

    update graphql schema

--- server/schema.js
+++ server/schema.js
@@ -2,7 +2,31 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
+    albums(ids: [ID]!): [Album]
+    album(id: ID!): Album
     hello: String
+  }
+
+  type Album {
+    id: ID!
+    name: String
+    artists: [Artist]
+    release_date: String
+    total_tracks: Int
+    images: [Image]
+  }
+
+  type Artist {
+    id: ID!
+    name: String
+    genres: [String]
+    images: [Image]
+  }
+
+  type Image {
+    height: Int
+    width: Int
+    url: String
   }
 `;
 
